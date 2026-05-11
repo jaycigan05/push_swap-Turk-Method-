4e223364b48e00c163fa791ac2e5af2fc8d86685
Jayci Gan <220996564+jaycigan05@users.noreply.github.com>
2026-05-11 09:39:48 +0800

    feat: main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jagan <jagan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 09:01:56 by jagan             #+#    #+#             */
-/*   Updated: 2026/05/11 09:07:16 by jagan            ###   ########.fr       */
+/*   Updated: 2026/05/11 09:38:36 by jagan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,27 @@ int	main(int argc, char **argv)
 {
 	t_stack *a;
 	t_stack *b;
+	int	size;
 	
 	if (argc < 2)
-		write(2, "Error\n", 6);
-		exit(1);
-	
+		return (0);
+	a = init_stack_a(argc, argv);
+	b = NULL;
+	if (is_sorted(a))
+	{
+		ft_free(&a);
+		return (0);
+	}
+	size = ft_stacksize(a);
+	if (size == 2)
+		sort_2(&a);
+	else if (size == 3)
+		sort_3(&a);
+	else if (size <= 5)
+		sort_5(&a, &b);
+	else if (size > 5)
+		sort_big(&a, &b);
+	ft_free(&b);
+	ft_free(&a);
+	return (0);
 }
