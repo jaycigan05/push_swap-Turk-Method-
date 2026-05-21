223e59e9032ced3373d35df1f3c951b3adc1631d
Jayci Gan <220996564+jaycigan05@users.noreply.github.com>
2026-05-21 12:10:45 +0800

    refactor: split sort_4, sort_5 and rotate_to_top into sort_fourfive.c

--- src/sort_small.c
+++ src/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: jagan <jagan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 10:27:48 by jagan             #+#    #+#             */
-/*   Updated: 2026/05/21 10:28:37 by jagan            ###   ########.fr       */
+/*   Updated: 2026/05/21 12:06:53 by jagan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,56 +54,4 @@ void	sort_3(t_stack **stack)
 	if (a->index < b->index && b->index < c->index)
 		return ;
 	sort_3_cases(stack, a, b, c);
-}
-
-/*
-rotate A until the node with target_index is at the top
-*/
-static void	rotate_to_top(t_stack **a, int target_index)
-{
-	int		size;
-	int		pos;
-	t_stack	*tmp;
-
-	size = ft_stacksize(*a);
-	pos = 0;
-	tmp = *a;
-	while (tmp->index != target_index)
-	{
-		pos++;
-		tmp = tmp->next;
-	}
-	if (pos <= size / 2)
-	{
-		while ((*a)->index != target_index)
-			ra(a);
-	}
-	else
-	{
-		while ((*a)->index != target_index)
-			rra(a);
-	}
-}
-
-void	sort_4(t_stack **a, t_stack **b)
-{
-	if (!a || !*a || !b)
-		return ;
-	rotate_to_top(a, ft_min_index(*a));
-	pb(a, b);
-	sort_3(a);
-	pa(a, b);
-}
-
-void	sort_5(t_stack **a, t_stack **b)
-{
-	if (!a || !*a || !b)
-		return ;
-	rotate_to_top(a, 0);
-	pb(a, b);
-	rotate_to_top(a, 1);
-	pb(a, b);
-	sort_3(a);
-	pa(a, b);
-	pa(a, b);
 }
